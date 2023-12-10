e93b69622e90521954113f03e1f2ed62ee51379f
ahmed assem <ahmed asam awage>
2023-12-10 17:28:34 +0200

    first1 commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const NotesApp());
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
