d3d57b19c5209277e1d53cc3a8aa9dde091352d8
ahmed assem <ahmed asam awage>
2023-12-14 19:48:46 +0200

    Custom Text Field

--- lib/views/widgets/add-note_bottomsheet.dart
+++ lib/views/widgets/add-note_bottomsheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 
 class AddNotesBottomSheet extends StatelessWidget {
   const AddNotesBottomSheet({Key? key}) : super(key: key);
@@ -10,9 +11,37 @@ class AddNotesBottomSheet extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: [
-          Text('Ahmed'),
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField()
         ],
       ),
     );
   }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Title',
+            hintStyle: TextStyle(color: kPrimaryColor),
+            border: buildBorder(),
+            enabledBorder: buildBorder(),
+            focusedBorder: buildBorder(kPrimaryColor)),
+      ),
+    );
+  }
+}
+
+OutlineInputBorder buildBorder([Color? color]) {
+  return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color ?? Colors.white));
 }
