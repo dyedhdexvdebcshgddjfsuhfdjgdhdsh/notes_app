ad5c430906054f6cba3d6f21b01ed7e0858517cf
ahmed assem <ahmed asam awage>
2023-12-15 18:23:44 +0200

    create note mddel

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String Date;
+  final int color;
+  NoteModel(
+      {required this.title,
+      required this.subtitle,
+      required this.Date,
+      required this.color});
+}
